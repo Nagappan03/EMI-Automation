9f09a1ef0c668dbdea558a88d1303da4de0224a9
Nagappan03 <karthiknano932@gmail.com>
2026-03-02 02:11:38 +0530

    fixed the spelling issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Layout from "./layouts/layout";
+import Layout from "./layouts/Layout";
 import Dashboard from "./pages/Dashboard";
 import Runs from "./pages/Runs";
 import System from "./pages/System";
